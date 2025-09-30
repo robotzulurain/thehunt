a85b316dca25d06f262aee7f2ec7853c1813e9f0
Harry Thabo Masuku <biomasuku@gmail.com>
2025-09-30 11:46:08 +0200

    Register utils app in INSTALLED_APPS

--- backend/amr_project/settings.py
+++ backend/amr_project/settings.py
@@ -7,6 +7,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*",'127.0.0.1','localhost']
 
 INSTALLED_APPS = [
+    'utils',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
